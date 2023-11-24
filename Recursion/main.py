4d7658f29495b93731f463462777d4c1d6835807
Ado4ka <ad04ka.ADa@gmail.com>
2023-11-24 15:31:34 +0300

    added changes

--- Recursion/main.py
+++ Recursion/main.py
@@ -4,14 +4,19 @@ def recursive_method(x):
     else:
         return recursive_method(x - 1) + 3
 
-result = recursive_method(2)
-print("Recursive method: ", result)
-
 def iterative_method(x):
     result = 0
     for _ in range(x):
         result += 3
     return result
 
-result = iterative_method(2)
+print("Multiplication by 3")
+x = int(input("Choose x: "))
+result = recursive_method(x)
+print("Recursive method: ", result)
+
+
+result = iterative_method(x)
 print("Iterative method: ", result)
+
+
